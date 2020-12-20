a0af918934bde628bfc7f03c00f7cbad4d11d706
Irina Skitayeva <irina.skitayeva@gmail.com>
2020-12-20 18:41:41 -0500

    fixing and testing

--- README.md
+++ README.md
@@ -1,41 +1,40 @@
-# frvc
- ## Description
-  frdc
- 
-  ## Table of Contents
-      * [Installation](#installation)
-    * [Usage](#usage)
-    * [Contributing](#contributers)
-    * [Tests](#tests)
-    * [Licences](#licenses)
-    
-  ## Installation
-  frd
-  
+# README Generator
 
+## Description
 
-    ## Usage
-    erdf
+This app will generate a professional README.md file anyone could use for their projects and it's based on quick prompts.
 
-    ## Credits
-    undefined
-    GitHub: [@fd](https://github.com/fd)
-    Email:  [fd](fd)
+## Table of Contents
 
-   ## Licences
-   This application is under the MIT license
-   
-    
-    https://choosealicense.com/licenses/MIT
-   true
-   
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributers)
+- [Tests](#tests)
+- [Licenses](#licenses)
 
-  
+## Installation
 
-     ## Tests
-     false
+To use that app the user should clone it to his machine
 
+## Usage
 
+This project is for anyone who wants a professional README.md file for his project!
 
+## Credits
 
+This application was developed by: Irina Skitayeva
+GitHub: [Irina256](https://github.com/Irina256)
+Email: [irina.skitayeva@gmail.com](irina.skitayeva@gmail.com)
 
+## Licenses
+
+This application is under the MIT license
+undefined
+![badge](https://img.shields.io/badge/license-MIT-blue);
+https://choosealicense.com/licenses/MIT
+
+## Tests
+
+Here is a screenshot of testing readme file generator
+![img1](./images/Screenshot 2020-12-20 173010.png)
+![img2](./images/Screenshot 2020-12-20 173352.png)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,13 +1,13 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 
-function renderLicenseBadge(license) {
-  if (!license) {
-    return "";
+const renderLicenseBadge = (license) => {
+  if (license) {
+    return `License: ${license};`;
   } else {
-    "![badge](https://img.shields.io/badge/license-" + license + "-blue)";
+    return "";
   }
-}
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -24,61 +24,79 @@ const generateInstallation = (installText) => {
     return "";
   } else {
     return `## Installation
-  ${installText}
+   ${installText}
+    `;
+  }
+};
+const generateTests = (testImages) => {
+  if (!testImages) {
+    return "";
+  } else {
+    return `Here is a screenshot of testing readme file generator
+    ![img1](./images/Screenshot 2020-12-20 173010.png)
+    ![img2](./images/Screenshot 2020-12-20 173352.png)
   `;
   }
 };
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {
+const renderLicenseSection = (license) => {
   if (!license) {
     return "";
   } else {
-    return true;
+    return `* [Licenses](#licenses)
+    `;
   }
-}
+};
+const installConfirm = (confirm) => {
+  if (!confirm) {
+    return "";
+  } else {
+    return `* [Installation](#installation)`;
+  }
+};
+const testsConfirm = (test) => {
+  if (!test) {
+    return "";
+  } else {
+    return `## Tests`;
+  }
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
- ## Description
+
+## Description
   ${data.description}
  
-  ## Table of Contents
-      * [Installation](#installation)
-    * [Usage](#usage)
-    * [Contributing](#contributers)
-    * [Tests](#tests)
-    * [Licences](#licenses)
-    
+## Table of Contents
+  ${installConfirm(data.installation)}
+  * [Usage](#usage)
+  * [Contributing](#contributers)
+  * [Tests](#tests)
+  ${renderLicenseSection(data.licenses)}
+  
   ${generateInstallation(data.installation)}
-
-
-    ## Usage
-    ${data.usage}
-
-    ## Credits
-    ${data.contributers}
-    GitHub: [@${data.github}](https://github.com/${data.github})
-    Email:  [${data.email}](${data.email})
-
-   ## Licences
-   This application is under the ${data.licenses} license
-   
-    ${renderLicenseBadge(data.licences)}
-    ${renderLicenseLink(data.licenses)}
-   ${renderLicenseSection(data.licenses)}
-   
-
   
 
-     ## Tests
-     ${data.tests}
+## Usage
+  ${data.usage}
 
+## Credits
+  This application was developed by: ${data.contributors}
+  GitHub: [${data.github}](https://github.com/${data.github})
+  Email:  [${data.email}](${data.email})
 
+## Licenses
+  This application is under the ${data.licenses} license
+  ${renderLicenseBadge(data.licenses)}
+  ![badge](https://img.shields.io/badge/license-${data.licenses}-blue);
+  ${renderLicenseLink(data.licenses)}
 
-
+  ${testsConfirm(data.tests)}
+  ${generateTests(data.tests)}
 
 `;
 }
